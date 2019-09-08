cc54e56de1c5d38517924c1f9ecca9320cbc41c0
Kevdagoat <kevdagoat@gmail.com>
2019-09-08 16:44:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from lib.apiget import APIAccess
 from lib.sql import SqlServer
 from flask import Flask, jsonify, abort, Response, request
 from datetime import datetime
+import json
 
 import configparser
 
@@ -11,8 +12,8 @@ config.read('api.conf')
 
 app = Flask(__name__)
 
-remote_api = APIAccess(config['tables']['cache_table'], config['db']['database'], config['db']['user'], config['db']['password'], config['db']['host'])
-db = SqlServer(config['db']['host'], config['db']['user'], config['db']['password'], config['db']['database'])
+remote_api = APIAccess(str(config['tables']['cache_table']), str(config['db']['database']), str(config['db']['user']), str(config['db']['password']), str(config['db']['host']))
+db = SqlServer(str(config['db']['host']), str(config['db']['user']), str(config['db']['password']), str(config['db']['database']))
 
 aol_url = config['external']['aol_api_query_url']
 
@@ -57,13 +58,20 @@ def save_animal_data():
 @app.route('/api/animals/get_all', methods=['GET'])
 def get_all_animals():
     json_dict = []
+    json_string = ""
     animal_sightings = db.get_all(animal_table)
     for row in animal_sightings:
         id, timedate, lat, lon, animal_type = row
         json_dict.append('{"id":%s,"animal_type":%s, "lat":%s, "lon":%s, "time":%s}'% (id, animal_type, lat, lon, timedate))
     record_number = len(json_dict)
-    json_response = '{"recordsFound":%s, "records":{%s}' % (record_number, str(json_dict).replace("'", ''))
-    return jsonify(json_response)
+    json_string = json_string + '['
+    for entry in json_dict:
+        json_string = json_string + entry
+        json_string = json_string + ','
+    json_string = json_string.strip(',')
+    json_string = json_string + ']'
+
+    return json_string
 
 @app.route('/api/poi/save', methods=['GET'])
 def save_poi():
@@ -82,32 +90,47 @@ def save_poi():
 @app.route('/api/poi/get_all', methods=['GET'])
 def get_all_pois():
     json_dict = []
+    json_string = ""
     pois = db.get_all(poi_table)
     for row in pois:
         id, code_type, name, lat, lon, timedate = row
-        json_dict.append('{"id":%s, "type":%s, "name":%s, "lat":%s, "lon":%s, "time":%s}'% (id, code_type, name, lat, lon, timedate))
+        json_dict.append('{"id":%s, "type":"%s", "name":"%s", "lat":%s, "lon":%s, "time":"%s"}' % (id, code_type, name, lat, lon, timedate))
     record_number = len(json_dict)
-    json_response = '{"recordsFound":%s, "records":{%s}' % (record_number, str(json_dict).replace("'", ''))
-    return jsonify(json_response)
+    json_string = json_string + '['
+    for entry in json_dict:
+        json_string = json_string + entry
+        json_string = json_string + ','
+    json_string = json_string.strip(',')
+    json_string = json_string + ']'
+    return json_string
+
 
 @app.route('/api/poi/search/by_lat', methods=['GET'])
 def find_poi_by_lat():
     lat = request.args.get('lat')
     json_dict = []
+    json_string = ""
     results = db.search_int(poi_table, 'lat', lat)
     print(results)
     for row in results:
         id, type_code, name, lat, lon, time = row
-        json_dict.append('{"id":%s, "type":%s, "name":%s, "lat":%s, "lon":%s, "time":%s}'% (id, type_code, name, lat, lon, time))
+        json_dict.append('{"id":%s, "type":"%s", "name":"%s", "lat":%s, "lon":%s, "time":"%s"}'% (id, type_code, name, lat, lon, time))
 
     record_number = len(json_dict)
-    json_response = '{"recordsFound":%s, "records":{%s}' % (record_number, str(json_dict).replace("'", ''))
-    return jsonify(json_response)
+    json_string = json_string + '['
+    for entry in json_dict:
+        json_string = json_string + entry
+        json_string = json_string + ','
+    json_string = json_string.strip(',')
+    json_string = json_string + ']'
+
+    return json_string
     
 @app.route('/api/poi/search/by_lon', methods=['GET'])
 def find_poi_by_lon():
     lon = request.args.get('lon')
     json_dict = []
+    json_string = ""
     results = db.search_int(poi_table, 'lon',  lon)
     print(results)
     for row in results:
@@ -115,8 +138,15 @@ def find_poi_by_lon():
         json_dict.append('{"id":%s, "type":%s, "name":%s, "lat":%s, "lon":%s, "time":%s}'% (id, type_code, name, lat, lon, time))
 
     record_number = len(json_dict)
-    json_response = '{"recordsFound":%s, "records":{%s}' % (record_number, str(json_dict).replace("'", ''))
-    return jsonify(json_response)
+    json_string = json_string + '['
+    for entry in json_dict:
+        json_string = json_string + entry
+        json_string = json_string + ','
+    json_string = json_string.strip(',')
+    json_string = json_string + ']'
+
+    return json_string
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
+
